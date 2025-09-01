b0e99a200242687c485315c203495cb2477553f4
Sajadsoftware <sajad.software96@gmail.com>
2025-09-02 02:56:16 +0330

    first commit

---
+++ frontend/src/pages/loginSignup.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const loginSignUp = () => {
+  return <div>Login / Sign Up Page</div>;
+};
+
+export default loginSignUp;
---
+++ frontend/src/pages/product.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const product = () => {
+  return <div>Product Page</div>;
+};
+
+export default product;
